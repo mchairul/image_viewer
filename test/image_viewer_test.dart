09a8b26bfe34521bd7f02642c9c414efcfa0576a
Muhammad Chairul <email.muhammad.chairul@gmail.com>
2024-07-20 15:50:23 +0700

    Initial commit

---
+++ test/image_viewer_test.dart
@@ -0,0 +1,10 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:image_viewer/image_viewer.dart';
+
+void main() {
+  test('adds one to input values', () {
+    const ImageViewer(url: 'https://st4.depositphotos.com/17828278/24401/v/450/depositphotos_244011872-stock-illustration-image-vector-symbol-missing-available.jpg',
+    height: 100);
+  });
+}
