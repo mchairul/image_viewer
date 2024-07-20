192e7ed328846f896f35e7cb8c24492371a839d7
Muhammad Chairul <email.muhammad.chairul@gmail.com>
2024-07-20 16:03:23 +0700

    change

--- test/webhozz_image_viewer_test.dart
+++ test/webhozz_image_viewer_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:image_viewer/image_viewer.dart';
+import 'package:webhozz_image_viewer/webhozz_image_viewer.dart';
 
 void main() {
   test('adds one to input values', () {
